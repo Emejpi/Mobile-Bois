f72b730c7ccca9d8363064917b6aaea058e0adff
MikBar2501 <45071328+MikBar2501@users.noreply.github.com>
2019-03-25 21:52:47 +0100

    LoadScreen
    Dodany Load screen z menu do game i game do menu. Trzeba sprawdzić działanie gdy będzie już więcej elementów na scenie

--- MobileTemplate/Assets/Menu/Scripts/Menu.cs
+++ MobileTemplate/Assets/Menu/Scripts/Menu.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
     public Image cover;
     public float fadeSpeed = 1;
+
+    public Slider slider;
+	public GameObject text;
+	public GameObject panel;
 
     public void Exit()
     {
@@ -17,11 +22,50 @@ public class Menu : MonoBehaviour
     {
         if(cover != null)
         StartCoroutine(FadeIn());
+
+        panel.SetActive(false);
+		text.SetActive(false);
+		slider.value = 0;
     }
 
     public void Play()
     {
-        LoadLevel(2);
+        //StartCoroutine(FadeOut());
+        //WaitSecondForFade(10,2);
+        cover.color = new Color(0, 0, 0, 255);;
+        LoadScene(2);
+    }
+
+    public void LoadScene(int sceneIndex) {
+		StartCoroutine(LoadAsynchronous(sceneIndex));
+	}
+
+    IEnumerator WaitSecondForFade(int time, int scene) {
+        
+        yield return new WaitForSeconds(time);
+        //LoadScene(scene);
+    }
+
+    IEnumerator LoadAsynchronous(int sceneIndex) {
+    AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+    //StartCoroutine(FadeOut());
+	panel.SetActive(true);
+	text.SetActive(false);
+	operation.allowSceneActivation = false;
+        while (!operation.isDone) {
+                float progress = Mathf.Clamp01(operation.progress / 0.9f);
+                Debug.Log("Loading progress: " + operation.progress + "%");
+                slider.value = progress;
+                if(progress >= 1f) {
+                    text.SetActive(true);
+                }
+                if(progress >= 1f && Input.touchCount > 0) {
+                    operation.allowSceneActivation = true;
+                    StartCoroutine(FadeIn());
+                }
+                
+                yield return null;
+            }  
     }
 
     public void Credits()
@@ -74,6 +118,26 @@ public class Menu : MonoBehaviour
 
         cover.color = new Color(0, 0, 0, 0);
         //cover.gameObject.SetActive(false);
+    }
+
+    IEnumerator FadeOut()
+    {
+        //cover.color = new Color(0,0,0,0);
+
+        float value = 0;
+
+        while(value < 1)
+        {
+            cover.color = new Color(0, 0, 0, value);
+            value += fadeSpeed * Time.deltaTime;
+
+            yield return null;
+            
+        }
+
+        cover.color = new Color(0, 0, 0, 1);
+        
+        
     }
 
 
